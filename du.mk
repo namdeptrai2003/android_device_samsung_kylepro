be9cd3c03f4d7d2ef5f4518cb78dec5cbf2f5172
DodoGTA GT <vwestlife20@gmail.com>
2016-06-17 18:14:32 +0300

    Add some forgotten stuff

--- du.mk
+++ du.mk
@@ -1,12 +1,15 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := cm_kylepro
+PRODUCT_NAME := du_kylepro
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7580
