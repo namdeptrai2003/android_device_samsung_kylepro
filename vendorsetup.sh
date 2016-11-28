namdeptrai2003/android_device_samsung_kyleprods

Update vendorsetup.sh for DU

vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-    add_lunch_combo cm_kyleprods-${i}
+    add_lunch_combo du_kyleprods-${i}
 done

namdeptrai2003/android_device_samsung_kylepro

vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-    add_lunch_combo cm_kylepro-${i}
+    add_lunch_combo du_kylepro-${i}
 done