c7c50eb2d1e919d5e8fbab82fdf4d1e5d52c0dae
ishantvivek <ishantvivek1311@gmail.com>
2017-01-31 19:15:32 +0000

    Revert "Build gello browser"
    This reverts commit 8f03decbf913d37c68160b57479b36bcd30ed77e.
    
    * They fixed it !!!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,9 +99,6 @@ TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS       := true
 # External apps on SD
 TARGET_EXTERNAL_APPS                        := sdcard1
 
-# Gello
-THEY_FIXED_GELLO                            := true
-
 # OpenGL
 BOARD_USES_HWCOMPOSER                       := true
 BOARD_USE_BGRA_8888                         := true
