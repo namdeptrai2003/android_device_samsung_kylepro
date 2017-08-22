0b18c2394079bc2cf06bddf63bfa4396ade8c29f
Real man born on semtember <hieunam03@gmail.com>
2017-08-22 17:56:41 +0700

    Update and rename viper.mk to aospa.mk

--- aospa.mk
+++ aospa.mk
@@ -1,6 +1,3 @@
-# Inherit some common stuff.
-$(call inherit-product, vendor/viper/config/common_full_phone.mk)
-
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
@@ -9,7 +6,7 @@ $(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := viper_kylepro
+PRODUCT_NAME := pa_kylepro
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7580
