edeb788771205fa8210e7fdb777f7cf597cb3df3
Real man born on semtember <hieunam03@gmail.com>
2017-08-23 14:01:54 +0700

    Update viper.mk

--- viper.mk
+++ viper.mk
@@ -1,9 +1,6 @@
 # Inherit some common stuff.
 $(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
 
