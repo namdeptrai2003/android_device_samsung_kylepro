3492a981f93d6f9b5b04aa5a442e6c1503344fb1
Real man born on semtember <hieunam03@gmail.com>
2019-05-19 08:34:38 +0700

    Unified 2 phones

--- lineage.mk
+++ lineage.mk
@@ -5,9 +5,9 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 $(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := lineage_kylepro
+PRODUCT_DEVICE := kyleproxx
+PRODUCT_NAME := lineage_kyleproxx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := GT-S7580
+PRODUCT_MODEL := GT-S758X
 PRODUCT_CHARACTERISTICS := phone
